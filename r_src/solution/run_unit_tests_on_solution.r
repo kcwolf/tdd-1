44806fc5d6416ed3bfd8b30110bb2c80f51b6751
Diamond Pickaxe <axe@Diamonds-iMac.local>
2013-09-18 11:39:37 -0400

    cleaned up solution directory

--- r_src/solution/run_unit_tests_on_solution.r
+++ r_src/solution/run_unit_tests_on_solution.r
@@ -1,6 +1,8 @@
 #### run_unit_tests_on_solution
-#### EXTRA:
+
 #### A completed version of the function is saved as r_src/quad_func_solution.r
-#### Load and test it like this:
-source('r_src/quadrant_func_solution.r')
+
+#### This will show all unit tests passing
+library(testthat)
+source('r_src/solution/quadrant_func_solution.r')
 test_dir('tests', reporter='Summary')  
