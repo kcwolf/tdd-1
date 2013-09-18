e7b42c32721ed271a20e3afb6c4e6117349856d7
tim-demos <stat22@openmailbox.org>
2013-09-18 11:51:28 -0400

    Update quadrant_func.r

--- r_src/quadrant_func.r
+++ r_src/quadrant_func.r
@@ -3,7 +3,9 @@
 quadrant_func <- function(x,y){
   # This function should take in two numbers, representing the coordinates of a point.
   # It should return 1, 2, 3, or 4, corresponding to the quadrant of the input point.
-  # Please include input checking.
+  # If the point is on the x or y axis, it should return 'x' or 'y' respectively.
+  # If the point is the origin, it should return 0.
+  # Please include input checking: i.e. make sure x and y are numeric.
   
   return("In Development")
 }
